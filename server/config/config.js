5e9021b5d8a7812196372e08dec2f4a7f4d125b4
damianSM1997 <damiansm97@gmail.com>
2020-05-31 04:36:29 -0500

    ya wey

--- server/config/config.js
+++ server/config/config.js
@@ -33,6 +33,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://damian:MI0E1zcZXQP20IwK@cluster0-naamb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
