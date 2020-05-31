51f5eadc6b01cfefc83878c1a6c8d14c8ae19253
damianSM1997 <damiansm97@gmail.com>
2020-05-31 04:10:58 -0500

    YA wey :C

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 // ============================
 //  Puerto
 // ============================
-process.env.PORT = process.env.PORT || 3000;
+process.env.PORT = process.env.PORT || 8000;
 
 
 // ============================
@@ -30,9 +30,9 @@ process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
 // ============================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = process.env.MONGO_URI;
-}
+//if (process.env.NODE_ENV === 'dev') {
+//    urlDB = 'mongodb://localhost:27017/cafe';
+//} else {
+urlDB = process.env.MONGO_URI;
+//}
 process.env.URLDB = urlDB;
